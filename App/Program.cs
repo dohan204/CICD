2a18c0a9a3de83bc73cfe349268605f0fd53ce3c
dohan204 <dohan20055@gmail.com>
2026-07-05 00:27:50 +0700

    pu

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+public class Product
+{
+    private decimal _price;
+    private string _name;
+    private string _description;
+    private string _priceType;
+
+
+    public decimal Price
+    {
+        get
+        {
+            return _price;
+        }
+
+        set
+        {
+            if(value == 0) 
+               throw new ArgumentNullException(nameof(value));
+
+            _price = value;
+        }
+    }
+}
