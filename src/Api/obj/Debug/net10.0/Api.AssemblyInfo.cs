5539fa384dccf4b6534ab1d1a8b61235b2e13107
dohan204 <dohan20055@gmail.com>
2026-07-06 13:31:08 +0700

    start

--- src/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc601c1e9702dcb863dd8d88d095bbe6952af2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f3604a176aab50c41c6076f4b32fa8ee8a734f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/Test/obj/Debug/net10.0/Test.AssemblyInfo.cs
+++ tests/Test/obj/Debug/net10.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc601c1e9702dcb863dd8d88d095bbe6952af2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f3604a176aab50c41c6076f4b32fa8ee8a734f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
